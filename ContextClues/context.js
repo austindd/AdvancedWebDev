3d126219fef70efad426e2ecc2fe17bca0275954
Austin Davis <austindanieldavis@gmail.com>
2018-11-06 17:30:47 -0600

    testing commit

--- ContextClues/context.js
+++ ContextClues/context.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
 
 
+    for (var i = 0; i < 100; i++) {
+        $('#main-container').append(`<h3 id='accusation${i + 1}'>Accusation ${i + 1}</h3>`);
+
+    }
+
 
 
 });
