7c81965df30631fb7bcad2a50b113b3ce00228ab
Austin Davis <austindanieldavis@gmail.com>
2018-11-07 14:28:30 -0600

    saving work

--- ES6 Drills/script.js
+++ ES6 Drills/script.js
@@ -14,16 +14,20 @@ $(document).ready(function () {
         let full_name = name.split(' ');
         console.log(full_name[0]);
     };
-    let getFirstName2 = (name) => console.log(name.split(' ')[0]);
+    let getFirstName2 = (name = 'FirstName') => console.log(name.split(' ')[0]);
 
     getFirstName();
     getFirstName2();
     getFirstName('Austin Daniel Davis');
     getFirstName2('Austin Daniel Davis');
 
-    
+    let doSomeMath = (a, b = 1) => ({
+        multiplify: (a * b),
+        exponentiate: Math.pow(a, b),
+    });
 
-
+    let math_result = doSomeMath(5, 9);
+    console.log(math_result);
 
 
     // Spread Syntax
