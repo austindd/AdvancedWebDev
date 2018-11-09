f5f858321f05691a39a1789545db9ad2b2c28c11
Austin Davis <austindanieldavis@gmail.com>
2018-11-09 05:10:17 -0600

    Almost done. Just squashing a bug slowly.

--- Dicey-Business/dicey.js
+++ Dicey-Business/dicey.js
@@ -1,63 +1,76 @@
 $(document).ready(function() {
 
-
     // =================== DOCUMENT-WIDE VARIABLES ===================
 
-    let die_count = 0;
     let die_object_map = [];
+    let recently_deleted = [];
+    let die_count = 0;
 
     // ========================== CLASSES ==========================
+
     class Die {
         constructor(name, value = Math.floor(Math.random() * 6) + 1) {
 
-            let self = this;  // saving 'this' context to a local variable in case 'this' changes during function execution
+            let thisDie = this;  // saving 'this' context to a local variable in case 'this' changes during function execution
 
             this.name = name;
             this.value = value;
             this.deletion_flag = false;
 
             $(`#die-container`).append(`<div id='${this.name}' class='btn die'>${this.value}</div>`);
-            this.element = $(`#die${die_count}`);
+            this.element = $(`#die${die_object_map.length}`);
 
-            this.element.click(function() {
-                if (self.deletion_flag == false) {
-                    $(self.element).addClass('ready-to-delete');
-                    self.deletion_flag = true;
-                } else if (self.deletion_flag == true) {
-                    $(self.element).removeClass('ready-to-delete');
-                    self.deletion_flag = false;
-                };
-            });
+
+            this.pushSelfToArray();
         };
+        pushSelfToArray() {
+        die_object_map.push(this);
+    }
         roll() {
             this.value = Math.floor(Math.random() * 6) + 1;
             console.log(this.value);
         };
     };
-
     console.log(Die);
 
-
     // ====================== EVENT LISTENERS/HANDLERS ======================
+
     $('#btn-generate').click(function() {
         let newDie = new Die(`die${die_count}`);
-        die_object_map[die_count] = newDie;
+        // die_object_map[die_object_map.length] = newDie;
         console.log(die_object_map);
         die_count++;
+
+        newDie.element.click(function() {
+            if (newDie.deletion_flag === false) {
+                $(newDie.element).addClass('ready-to-delete');
+                newDie.deletion_flag = true;
+            } else if (newDie.deletion_flag === true) {
+                $(newDie.element).removeClass('ready-to-delete');
+                newDie.deletion_flag = false;
+            };
+        });
+
         return;
     });
     $('#btn-roll').click(function() {
-        die_object_map.forEach(function(i) {
+        die_object_map.forEach(function(item) {
             i.roll();
             i.element.text(i.value);
         });
         return;
     });
     $('#btn-remove').click(function() {
-        
-        return;
+        $('.ready-to-delete').remove();
+        let item;
+        die_object_map.forEach(function(item, i) {
+            if(item.deletion_flag === true) {
+                die_object_map.splice(i, 1);
+                i--;
+            };
+        });
+        die_count--;
     });
-
     // ======================== HELPER FUNCTIONS ========================
 
 });
