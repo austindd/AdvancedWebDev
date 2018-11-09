f4efcff8e6916f4effc4b69e244cb89345659e5a
Austin Davis <austindanieldavis@gmail.com>
2018-11-09 16:14:46 -0600

    found solution for removing deleted dice from the array of dice objects.

--- Dicey-Business/dicey.js
+++ Dicey-Business/dicey.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     // =================== DOCUMENT-WIDE VARIABLES ===================
 
     let die_object_map = [];
-    let recently_deleted = [];
     let die_count = 0;
 
     // ========================== CLASSES ==========================
@@ -12,14 +11,24 @@ $(document).ready(function() {
         constructor(name, value = Math.floor(Math.random() * 6) + 1) {
 
             let thisDie = this;  // saving 'this' context to a local variable in case 'this' changes during function execution
-
             this.name = name;
+            this.id = die_count;
             this.value = value;
             this.deletion_flag = false;
 
-            $(`#die-container`).append(`<div id='${this.name}' class='btn die'>${this.value}</div>`);
-            this.element = $(`#die${die_object_map.length}`);
+            $(`#die-container`).append(`<div id='${this.id}' class='btn die'>${this.value}</div>`);
+            this.element = $(`#${this.id}`);
 
+            this.element.click(() => {
+                if (this.deletion_flag === false) {
+                    $(this.element).addClass('deletion-flag');
+                    this.deletion_flag = true;
+                } else if (this.deletion_flag === true) {
+                    $(this.element).removeClass('deletion-flag');
+                    this.deletion_flag = false;
+                };
+            });
+    
 
             this.pushSelfToArray();
         };
@@ -40,36 +49,46 @@ $(document).ready(function() {
         // die_object_map[die_object_map.length] = newDie;
         console.log(die_object_map);
         die_count++;
-
-        newDie.element.click(function() {
-            if (newDie.deletion_flag === false) {
-                $(newDie.element).addClass('ready-to-delete');
-                newDie.deletion_flag = true;
-            } else if (newDie.deletion_flag === true) {
-                $(newDie.element).removeClass('ready-to-delete');
-                newDie.deletion_flag = false;
-            };
-        });
-
         return;
     });
     $('#btn-roll').click(function() {
         die_object_map.forEach(function(item) {
-            i.roll();
-            i.element.text(i.value);
+            item.roll();
+            item.element.text(item.value);
         });
         return;
     });
     $('#btn-remove').click(function() {
-        $('.ready-to-delete').remove();
-        let item;
-        die_object_map.forEach(function(item, i) {
-            if(item.deletion_flag === true) {
-                die_object_map.splice(i, 1);
-                i--;
-            };
-        });
-        die_count--;
+
+        let removed_dice = die_object_map.filter( (val) => {return (val.deletion_flag === true)});
+        
+        //  While (there is still any dice flagged for deletion in the array) {
+        //      - Find the index of the first flagged instance in the array.
+        //      - Remove that one flagged instance from array.
+        //  }
+        while (die_object_map.some((val) => {return (val.deletion_flag === true)})) {
+            let myIndex = die_object_map.findIndex((val) => {return (val.deletion_flag === true)});
+            die_object_map.splice(myIndex, 1);
+            console.log('Culling die_object_map', die_object_map);
+        };
+
+
+
+
+
+
+        console.log('removed_dice: ', removed_dice);
+
+        $('.deletion-flag').remove();
+
+        // for(i = die_object_map; i > 0; i--) {
+        //     if(i.deletion_flag === true) {
+        //         die_object_map.splice(i, 1);
+                
+        //     };
+        // };
+        console.log(die_object_map);
+
     });
     // ======================== HELPER FUNCTIONS ========================
 
