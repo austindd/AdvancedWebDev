fee18c71cc2248e9dd29a8f3a6cbc99873c6485e
Austin Davis <austindanieldavis@gmail.com>
2018-11-15 16:25:38 -0600

    finished callbacks section

--- Async-Drills/script.js
+++ Async-Drills/script.js
@@ -4,26 +4,34 @@ $(document).ready(function () {
     // ================= Async: Now and Later =================
 
     var num_1_global
-    let firstFunction = (msg, num) => {
+    const firstFunction = (msg, num) => {
         console.log(msg);
         num_1_global = num
         return num;
-    }
-    firstFunction('Hey there!', 5);
-    let secondFunction = () => {
+    };
+    const secondFunction = () => {
         let result = 4 * num_1_global;
         console.log(result);
-    }
-    let myTimeout = () => {
+    };
+    const myTimeout = () => {
         setTimeout(secondFunction, 2000);
-    }
-    secondFunction();
-    myTimeout();
+    };
+
+
+    $(`#button1`).click(() => {
+        firstFunction('Hey there!', 5);
+        secondFunction();
+        myTimeout();
+    });
+
+
 
 
     // ====================== Callbacks ======================
 
-    let getWords = () => {
+
+    // I don't know if this was the intended method, but I just kinda made it work...ish
+    const getWords = () => {
         let word1 = "Pleased";
         let word2 = 'to';
         let word3 = 'meet';
@@ -51,16 +59,32 @@ $(document).ready(function () {
         };
         getFirstWord();
     };
-    getWords();
+    $(`#button2`).click(getWords);
 
 
-const countdown = (num, callback) => {
+    const countdown = (num, callback = done) => {
+        let duration = num; // Saving the initial 'num' value to pass it into the callback function.
+        const internalTimer = (i, internalCallback) => {
+            if (i > 0) {
+                setTimeout(() => {
+                    console.log(i);
+                    internalTimer(i - 1, internalTimer);
+                }, 1000);
+            } else {
+                setTimeout(() => {
+                    return callback(duration);
+                }, 1000);
+            };
+        };
+        internalTimer(num, callback);
+    };
 
-}
-
-
-
-
+    const done = (n) => {
+        console.log(`Done-ion rings! after ${n} seconds.`);
+    };
+    $(`#button3`).click(() => {
+        countdown(5, done)
+    });
 
 
 
