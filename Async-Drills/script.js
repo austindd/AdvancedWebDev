2037fcb40ce6fdc739d51b59089ae0ed50a0dccd
Austin Davis <austindanieldavis@gmail.com>
2018-11-16 01:05:33 -0600

    Finished Async Drills Lab.

--- Async-Drills/script.js
+++ Async-Drills/script.js
@@ -82,12 +82,100 @@ $(document).ready(function () {
     const done = (n) => {
         console.log(`Done-ion rings! after ${n} seconds.`);
     };
+
     $(`#button3`).click(() => {
         countdown(5, done)
     });
 
 
+    // ========================= Promises =========================
+
+    var my_var = true;
+
+    let orderingChickenSandwich = () => {           // Had to wrap this promise in a function because otherwise it runs on page load
+        return new Promise((resolve, reject) => {   // I only want this to run when I invoke the orderFood() function below.
+            if (my_var === true) {
+                let myOrder = {
+                    sandwich: 'chicken',
+                    veggies: 'lettuce'
+                };
+                let success = [myOrder, 'Would you like fries with that?'];
+                resolve(success);
+            };
+            if (my_var === false) {
+                let err = new Error('Suit yourself...');
+                reject(err);
+            };
+        });
+    };
+
+    const orderFood = () => {
+        orderingChickenSandwich().then((result) => {
+            let [order, message] = result;
+            console.log(order);
+            console.log(message);
+        }, (err) => {
+            console.log(err);
+        }).catch((err) => {
+            console.log(err, `I guess you'll be having a knuckle sandwich, then!`);
+        });
+    };
+
+    $(`#button4`).click(orderFood);
 
 
+    // ====================== Chaining Promises ======================
+
+    let startChain = (num) => {
+        let start_value = num
+        let err = () => new Error('Something AWFUL happened!');
+        return new Promise((resolve, reject) => {
+            let val = start_value
+            setTimeout(() => {
+                let x1 = val;
+                console.log(val);
+                resolve(x1);
+                reject(err);
+            }, 500);
+        }).then((val) => {
+            return new Promise((resolve, reject) => {
+                let x = val;
+                setTimeout(() => {
+                    let x2 = 2 * x;
+                    console.log(x2);
+                    resolve(x2);
+                    reject(err);
+                }, 500);
+            }).then((val) => {
+                return new Promise((resolve, reject) => {
+                    let x = val;
+                    setTimeout(() => {
+                        let x3 = 4 * x;
+                        console.log(x3);
+                        resolve(x3);
+                        reject(err);
+                    }, 500);
+                }).then((val) => {
+                    return new Promise((resolve, reject) => {
+                        let x = val;
+                        setTimeout(() => {
+                            let x4 = 6 * x;
+                            console.log(x4);
+                            resolve(x4);
+                            reject(err);
+                        }, 500);
+                    }).then((val) => {
+                        console.log('Done-ion Rings!');
+                        return val;                         // Final value returned here
+                    }).catch(err)
+                }).catch(err)
+            }).catch(err)
+        }).catch(err)
+    };
+
+$(`#button5`).click(() => {
+    console.log('Starting Chain...')
+    startChain(1);
+});
 
 });
